8ce097d7a5facf764e6e459f3bb60b3c205ef691
Oleg Arefyev <arefyev.o.a@gmail.com>
2025-05-06 14:22:04 +0300

    feat: implement environment variables overwriting

--- scripts/ollama_cors.sh
+++ scripts/ollama_cors.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# This script sets the OLLAMA_ORIGINS environment variable for the ollama service
-# AFTER INSTALL OR UPDATE OLLAMA.
+# This script sets the OLLAMA environment variable for the ollama service
 #
-# 1. Define the OLLAMA_ORIGINS variable with the appropriate value.
+# 1. WARNING! Define the OLLAMA_ORIGINS variable with the appropriate value.
 # 2. Ensure you have root privileges. The script will prompt you to run as root if necessary.
-# 3. The script will check if the environment variables are already set in the ollama.service file.
-# 4. If the variables are not set, the script will add them under the [Service] section.
+# 3. The script will remove OLLAMA environment variables are already set in the ollama.service file.
+# 4. The script will then add environment variables under the [Service] section.
 # 5. The script will then reload the systemd daemon and restart the ollama service.
 #
 # Example usage:
 # ./set_ollama_origins.sh
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
+    echo "Run as root"
     sudo "$0"
     exit 0
 fi
@@ -30,22 +29,28 @@ RESET=$(tput sgr0)
 OLLAMA_ORIGINS="http://192.168.1.*"
 OLLAMA_HOST="http://0.0.0.0:11434"
 
-cd /etc/systemd/system || { echo "Failed to change directory"; exit 1; }
+env_vars=(
+    "Environment=\"OLLAMA_ORIGINS=$OLLAMA_ORIGINS\""
+    "Environment=\"OLLAMA_HOST=$OLLAMA_HOST\""
+)
 
-if grep -q "OLLAMA_ORIGINS" ollama.service; then
-    echo "${RED}Environment variables already exist${RESET}"
-    exit 0
-else
-    env_vars=(
-        "Environment=\"OLLAMA_ORIGINS=$OLLAMA_ORIGINS\""
-        "Environment=\"OLLAMA_HOST=$OLLAMA_HOST\""
-    )
+cd /etc/systemd/system || { echo "${RED}Failed to change directory${RESET}"; exit 1; }
 
-    for env_var in "${env_vars[@]}"; do
-        echo "$env_var added"
-        sudo sed -i "/\[Service\]/a\\$env_var" ollama.service
-    done
-
-    sudo systemctl daemon-reload && sudo systemctl restart ollama.service
-    echo "${GREEN}Ollama service restarted${RESET}"
+if [ ! -f ollama.service ]; then
+    echo "${RED}Error: ollama.service not found${RESET}"
+    exit 1
 fi
+
+# remove environment variables
+sed -i '/Environment="OLLAMA_ORIGINS=/d' ollama.service
+sed -i '/Environment="OLLAMA_HOST=/d' ollama.service
+
+# add environment variables
+for env_var in "${env_vars[@]}"; do
+    echo "$env_var added"
+    sudo sed -i "/\[Service\]/a\\$env_var" ollama.service
+done
+
+sudo systemctl daemon-reload && sudo systemctl restart ollama.service
+echo "${GREEN}Ollama service restarted${RESET}"
+
